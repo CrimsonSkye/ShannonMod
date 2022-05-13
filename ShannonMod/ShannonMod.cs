653274c4f046fba5917ffb9e5552c6f6b7c194c4
Anxiety Pealope <shannon33.west@gmail.com>
2022-05-12 19:57:54 -0500

    Initial Commit

---
+++ ShannonMod/ShannonMod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Vintagestory.API.Common;
+
+namespace ShannonMod
+{
+    public class ShannonMod : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+    }
+}
