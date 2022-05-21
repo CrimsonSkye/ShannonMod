8b093197d585b9b171c0629adb903b7ae5afde6c
Anxiety Pealope <shannon33.west@gmail.com>
2022-05-20 19:29:16 -0500

    Start Baking Pan

---
+++ ShannonMod/BlockEntities/BEBakingPan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vintagestory.API.Common;
+
+namespace ShannonMod.BlockEntities
+{
+    public class BEBakingPan : BlockEntity
+    {
+    }
+}
